8e3ad6a4f4966a2bb9e132268a91f61b0ddf4ecc
AlistairJMatthews <85308596+AlistairJMatthews@users.noreply.github.com>
2022-05-24 22:22:25 +0100

    Jupyter Notebook and Config Files
    PSD files are too big to upload.

---
+++ plot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+pycbc_inference_plot_posterior \
+    --input-file inferenceff.hdf \
+    --output-file inferenceff.png \
+    --z-arg snr 
---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+# configuration files
+PRIOR_CONFIG=model_debug-config.ini
+DATA_CONFIG=data_debug-config.ini
+SAMPLER_CONFIG=sampler_debug-config.ini
+
+OUTPUT_PATH=inference.hdf
+
+# the following sets the number of cores to use; adjust as needed to
+# your computer's capabilities
+NPROCS=8
+
+# run sampler
+# Running with OMP_NUM_THREADS=1 stops lalsimulation
+# from spawning multiple jobs that would otherwise be used
+# by pycbc_inference and cause a reduced runtime.
+OMP_NUM_THREADS=1 \
+pycbc_inference --verbose \
+    --seed 18934 \
+    --config-file ${PRIOR_CONFIG} ${DATA_CONFIG} ${SAMPLER_CONFIG} \
+    --output-file ${OUTPUT_PATH} \
+    --nprocesses ${NPROCS} \
+    --force
